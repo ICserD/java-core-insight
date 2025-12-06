4311797548abf176d9ce9ae872a76f1a78e40394
ICserD <20211333043@m.scnu.edu.cn>
2025-12-06 17:01:46 +0800

    feat(concurrent): implement a thread safe demo for module `Account` through cas

--- src/main/java/concorrent/cas/TestAccount.java
+++ src/main/java/concorrent/cas/TestAccount.java
@@ -1,11 +1,15 @@
 package concorrent.cas;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TestAccount {
     public static void main(String[] args) throws InterruptedException {
         Account accountUnSafe = new AccountUnsafe(10000);
         Account accountSynchronizedSafe = new AccountSynchronizedSafe(10000);
+        Account accountCasSafe = new AccountCasSafe(new AtomicInteger(10000));
 
         Account.demo(accountUnSafe);
         Account.demo(accountSynchronizedSafe);
+        Account.demo(accountCasSafe);
     }
 }
